eee84703abc168e390d135e6dedc9e002e3ff0f5
Karlsson0214 <karlsson0214@gmail.com>
2021-01-24 16:25:03 +0100

    comment about sorting

--- DrillWpfHighScore/Result.cs
+++ DrillWpfHighScore/Result.cs
@@ -22,8 +22,17 @@ namespace DrillWpfHighScore
         }
 
         // lägg till ToString() eller property Name
+
+        
         public int CompareTo([AllowNull] Result other)
         {
+            // Metoden CompareTo behövs för att implementera interfacet IComparable.
+            // Denna metod används sedan när listan sorteras.
+            // Sorteras på score. Störst kommer först.
+            // Metoden returnerar -1 om detta (this) objektet är större än det andra objektet (other)
+            // och annars returneras 1
+            // null-värde hamnar sist i listan. Vi bör dock inte ha null-värden i listan.
+
             if (other == null)
             {
                 // this is first
