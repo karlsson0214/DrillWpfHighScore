cd9f3b3602712fce8b12204229fe9c2d5c1b7733
Karlsson0214 <karlsson0214@gmail.com>
2021-01-24 15:28:11 +0100

    Add project files.

---
+++ DrillWpfHighScore/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DrillWpfHighScore
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        // lista med poäng
+        private List<Result> scores = new List<Result>();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SaveScore_Click(object sender, RoutedEventArgs e)
+        {
+            // spara resultat i listan scores
+
+            // sortera listan
+            scores.Sort();
+
+            // skriv ut top 5 som 
+            // 1. Ada 1060
+            // 2. Beda 980
+            // ...
+            // 5. ...
+
+        }
+    }
+}
---
+++ DrillWpfHighScore/Result.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace DrillWpfHighScore
+{
+    class Result : IComparable<Result>
+    {
+        private int score;
+        // lägg till name
+
+        // lägg till konstruktor
+
+        // property
+        public int Score
+        {
+            get
+            {
+                return score;
+            }
+        }
+
+        // lägg till ToString() eller property Name
+        public int CompareTo([AllowNull] Result other)
+        {
+            if (other == null)
+            {
+                // this is first
+                return 1;
+            }
+            else if (Score > other.Score)
+            {
+                // this is first
+                return 1;
+            }
+            else
+            {
+                // other is first
+                return -1;
+            }                
+        }
+    }
+}
